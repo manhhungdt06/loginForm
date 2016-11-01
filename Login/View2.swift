f1455714d388911f1b686586fd9d18d103467f8d
manhhung.dt6 <manhhung.dt6@gmail.com>
2016-11-01 17:50:02 +0700

    update

---
+++ Login/View2.swift
@@ -0,0 +1,38 @@
+//
+//  View2.swift
+//  Login
+//
+//  Created by techmaster on 11/1/16.
+//  Copyright © 2016 techmaster. All rights reserved.
+//
+
+import UIKit
+
+class View2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func showList(_ sender: UIButton) {
+        
+    }
+}
--- Login/ViewController.swift
+++ Login/ViewController.swift
@@ -12,17 +12,73 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tf_user: UITextField!
     @IBOutlet weak var tf_password: UITextField!
-    var users = ["hung":"123456", "hung1":"123", "hung2":"567"]
+    @IBOutlet weak var addUserBtn: UIButton!
+    @IBOutlet weak var editUserBtn: UIButton!
+    @IBOutlet weak var removeUserBtn: UIButton!
+    var users = ["admin":"admin", "hung1":"123", "hung2":"567"]
     override func viewDidLoad() {
         super.viewDidLoad()
+        editUserBtn.isHidden = true
+        removeUserBtn.isHidden = true
     }
 
+    @IBAction func addUser(_ sender: UIButton) {
+        print("users.count = \(users.count)")
+        if tf_user.text != "" {
+            users[tf_user.text!] = tf_password.text!
+            print("users.count = \(users.count)")
+            print(users)
+        } else {
+            print("input something")
+        }
+    }
+    
+    @IBAction func editUser(_ sender: UIButton) {
+        if tf_user.text != "" {
+            if users[tf_user.text!] == nil {
+                print("\(tf_user.text!) not exist")
+            } else {
+                users[tf_user.text!] = tf_password.text!
+                print("key-value after update: \(tf_user.text!) and \(tf_password.text!)")
+                print(users)
+            }
+        } else {
+            print("input something")
+        }
+    }
+    
+    @IBAction func removeUser(_ sender: UIButton) {
+        if tf_user.text != "" {
+            if users[tf_user.text!] == nil {
+                print("\(tf_user.text!) not exist")
+            } else {
+                if tf_user.text! == "admin" {
+                    print("can't remove")
+                } else {
+                    users.removeValue(forKey: tf_user.text!)
+                    print("users.count = \(users.count)")
+                    print(users)
+                }
+            }
+        } else {
+            print("input something")
+        }
+    }
+    
     @IBAction func action_login(_ sender: AnyObject) {
         if let password = users[tf_user.text!]
         {
             if password == tf_password.text
             {
                 print("Đăng nhập thành công")
+                // addUserBtn.isHidden = false
+                editUserBtn.isHidden = false
+                if users[tf_user.text!] == "admin" {
+                    removeUserBtn.isHidden = false
+                } else {
+                    print("normal user")
+                }
+                
             }
             else
             {
